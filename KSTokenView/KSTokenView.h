7c7c39ac5d33bdddde87d0cd9b98bd60fbdb8bcd
Evan Coleman <e@edc.me>
2019-03-31 16:34:54 -0400

    Update for swift 5

---
+++ KSTokenView/KSTokenView.h
@@ -0,0 +1,19 @@
+//
+//  KSTokenView.h
+//  KSTokenView
+//
+//  Created by Evan Coleman on 10/18/19.
+//  Copyright © 2019 Evan Coleman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KSTokenView.
+FOUNDATION_EXPORT double KSTokenViewVersionNumber;
+
+//! Project version string for KSTokenView.
+FOUNDATION_EXPORT const unsigned char KSTokenViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KSTokenView/PublicHeader.h>
+
+
